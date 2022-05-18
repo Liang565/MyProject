1cb638757a10f1d5b69a6fe4b7b20d7f85bc10e0
liang <2689989345@qq.com>
2022-05-18 15:25:52 +0800

    完成部分权限

--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -18,7 +18,7 @@ const model = TypegooseModule.forFeature(models);
     TypegooseModule.forRootAsync({
       useFactory() {
         return {
-          uri: 'mongodb://localhost/blogs',
+          uri: 'mongodb://localhost:27018/blogs',
         };
       },
     }),
--- server/prod/apps/admin/main.js
+++ server/prod/apps/admin/main.js
@@ -1507,7 +1507,7 @@ DbModule = __decorate([
             nestjs_typegoose_next_1.TypegooseModule.forRootAsync({
                 useFactory() {
                     return {
-                        uri: 'mongodb://localhost/blogs',
+                        uri: 'mongodb://localhost:27018/blogs',
                     };
                 },
             }),
